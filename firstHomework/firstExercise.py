236b48c86be0bf24b0845d9057f28310d7c9260d
Oleksandr <sansanychmartynko@gmail.com>
2021-09-08 17:00:54 +0300

    fixes

---
+++ firstHomework/firstExercise.py
@@ -0,0 +1,8 @@
+def first_exercise():
+    number_input = float(input('input some number: '))
+    print(number_input)
+    string_input = input('input some string: ')
+    print(string_input)
+
+
+first_exercise()
